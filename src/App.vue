77b251aef104301bc4e43367825ba63c22874dc5
soulpirata <ngaihawma97@gmail.com>
2024-07-23 17:39:25 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+ 
+  <div class="absolute w-[12%] top-0 mt-[34px] ml-[180px] left-0 mr-[70px] overflow-hidden">
+  <img src="./assets/logo.png">
+  </div>
+
+  <div class="cursor-pointer text-center mt-12 flex gap-10 justify-center text-[25px]">
+    <a ref="Home">Home</a>
+    <a ref="About Us">About Us</a>
+    <a ref="Find centre">Find Centre</a>
+    <a ref="Contact Us">Home</a>
+  </div>
+  
+  <div class="absolute w-[35%] mt-15 right-0 mr-[70px] overflow-hidden h-[820px]">
+  <img src="./assets/hand.png">
+  </div>
+  <div class="text-[100px] mt-[230px] ml-40 font-Roboto font-bold">
+    Your Partner in <p class="mt-[-20px]"> Health and Wellness</p>
+  </div>
+<div class="mt-[25px] text-[30px] ml-40">
+  <p>
+    We are committed to providing you with the best medical and <p> healthcare services to help you live healthier and happier.</p>
+  </p>
+</div>
+</div>  
+</template>
+
