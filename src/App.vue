7e7124ebaf488c952f59556b8320d329aab3df92
soulpirata <ngaihawma97@gmail.com>
2024-07-24 17:34:57 +0530

    thlalak ka dah lut

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,66 @@
   <img src="./assets/logo.png">
   </div>
 
-  <div class="cursor-pointer text-center mt-12 flex gap-10 justify-center text-[25px]">
+  <div class="cursor-pointer text-center mt-12 flex gap-10 justify-center text-[20px]">
     <a ref="Home">Home</a>
     <a ref="About Us">About Us</a>
     <a ref="Find centre">Find Centre</a>
-    <a ref="Contact Us">Home</a>
+    <a ref="Contact Us">Contact Us</a>
   </div>
   
   <div class="absolute w-[35%] mt-15 right-0 mr-[70px] overflow-hidden h-[820px]">
   <img src="./assets/hand.png">
   </div>
-  <div class="text-[100px] mt-[230px] ml-40 font-Roboto font-bold">
-    Your Partner in <p class="mt-[-20px]"> Health and Wellness</p>
+
+  
+
+  <div class="text-[100px] mt-[200px] ml-40 font-Roboto font-bold">
+  Your Partner in <p class="mt-[-20px]"> Health and Wellness</p>
   </div>
-<div class="mt-[25px] text-[30px] ml-40">
+
+  <div class="mt-[25px] text-[30px] ml-40">
   <p>
     We are committed to providing you with the best medical and <p> healthcare services to help you live healthier and happier.</p>
   </p>
+  </div>
+
+  <div class="w-full flex justify-center mt-14">
+      <div class="bg-white h-[200px] w-[75%] mt-[100px] z-10 rounded-[30px] border-solid border-4 border-[rgb(175,227,255)] shadow-lg flex items-center justify-evenly ">
+        <div class="flex w-[400px]">   
+          <img class="w-[5rem]" src="./assets/doctor.png">
+          <div>
+          <p class="ml-[20px] text-[24px] font-bold">Doctors & Health Workers</p> <p class="ml-[20px] text-[34px] font-semibold text-[274760]">13</p></div>
+        </div>
+        <div class="flex  w-[350px]">
+          <img class="w-[5rem]" src="./assets/services.png">
+          <div>
+          <p class="ml-[20px] text-[24px] font-bold">Services</p> <p class="ml-[20px] text-[34px] font-semibold text-[274760]">24*7</p></div>
+        </div>
+        <div class="flex  w-[400px]">
+          <img class="w-[5rem]" src="./assets/location.png">
+          <div ><p class="ml-[20px] text-[24px] font-bold">Locaion</p><p class="ml-[20px] text-[34px] font-semibold text-[274760]">Zuangtui, Aizawl</p></div>
+        </div>
+      </div>
 </div>
+
+
+  <div>
+      <div class="absolute w-[25%] mt-[200px] ml-[180px] overflow-hidden">
+      <img src="./assets/hospital.png">
+      </div>
+  </div>
+
+  <div class="text-[40px] mt-[300px] font-Roboto font-bold ml-[800px]">
+  About us 
+  <p class="text-[30px] ml-10 text-[rgb(48,123,196)] font-medium mt-2">The quick brown fox jump over the lazy dog.</p>
+  <p class="text-[25px] font-normal ml-10 mt-5 w-[80%] text-[rgb(142,159,172)]">The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.The quick brown fox jump over the lazy dog.</p>
+
+  </div>
+
+
+
+
+  
 </div>  
 </template>
 
